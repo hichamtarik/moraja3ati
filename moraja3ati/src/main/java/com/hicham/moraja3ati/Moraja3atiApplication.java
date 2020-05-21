84fd72a44a5eae367e9fe715025ca006961283f7
hicham tarik <elghazihichame@gmail.com>
2020-05-21 21:34:15 +0000

    initial commit . create spring boot project and cofigure application properties

---
+++ moraja3ati/src/main/java/com/hicham/moraja3ati/Moraja3atiApplication.java
@@ -0,0 +1,13 @@
+package com.hicham.moraja3ati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moraja3atiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moraja3atiApplication.class, args);
+	}
+
+}
---
+++ moraja3ati/src/test/java/com/hicham/moraja3ati/Moraja3atiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hicham.moraja3ati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Moraja3atiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
